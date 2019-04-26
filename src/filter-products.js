685dbd9c32abe319a43d4f49ccd9d276fdee6459
AustinLeeGordon <gordon.austin2@gmail.com>
2019-04-26 12:39:48 -0500

    Check for lowercase sku when filtering

--- src/filter-products.js
+++ src/filter-products.js
@@ -1,7 +1,7 @@
 function filterProducts(productUrls, skus) {
     return productUrls.filter(url => {
         for (let i = 0; i < skus.length; i++) {
-            let sku = skus[i];
+            let sku = skus[i].toLowerCase();
             if (url.indexOf(`/${sku}/`) !== -1) {
                 return true;
             }
